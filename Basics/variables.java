0c2a73b46bbe784d9be71613956ac1dd097e0e8f
Pushpak003 <pushpakpathe63@gmail.com>
2026-02-10 14:13:30 +0530

    variables

---
+++ Basics/variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int number = 10;
+        String message = "The number is: ";
+        double decimalValue = 5.5;
+        char letter = 'A';
+        boolean isTrue = true;
+
+        // Printing the variable values
+        System.out.println(message + number + decimalValue + letter + isTrue);
+    }
+}
