81e99bfb636c47a424ddc93e9e7468d8bc5589fe
Pushpak003 <pushpakpathe63@gmail.com>
2026-02-10 14:17:09 +0530

    io

--- Basics/io.java
+++ Basics/io.java
@@ -5,5 +5,6 @@ public class io {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
